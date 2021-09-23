73d04fd934df2b3474d6358dba2c89377992c388
TanjaGagic <57789556+TanjaGagic@users.noreply.github.com>
2021-09-23 18:04:14 +0200

    added documentation

--- reactjs/src/components/pages/Reviews.js
+++ reactjs/src/components/pages/Reviews.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import '../../App.css';
 
+
+
 export default function Reviews() {
-  return <h1 className='offers'>REVIEW</h1> ;
-}
+
+
+  
+  return ( <h1 className='offers'>REVIEW</h1> );
+  }
